b4fc3cbcb852ac63771f55f0722d933298b9a5ae
Luka Đurišić <luka.durisic123@gmail.com>
2025-04-16 12:06:55 +0200

    Deployment fix

--- src/app/components/CheckpointCard/CheckpointCard.tsx
+++ src/app/components/CheckpointCard/CheckpointCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./CheckpointCard.module.css";
-import Image from "next/image";
 import LocationIcon from "../../assets/location";
 
 interface CheckpointCardData {
