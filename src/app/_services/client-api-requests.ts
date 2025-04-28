8903a262bf7657dd353f3d8697cb4d90b2aa2fd3
Luka Đurišić <luka.durisic123@gmail.com>
2025-04-28 13:04:03 +0200

    Api calls fix

--- src/app/_services/client-api-requests.ts
+++ src/app/_services/client-api-requests.ts
@@ -5,6 +5,7 @@ export enum API_PATHS {
   /* I used https://api.restful-api.dev just to test the hook and it has /objects path */
   tours = "/objects",
   login = "/accounts/login/",
+  routes = "/routes/",
 }
 
 //TODO add zod validation and env.mjs for handling env vars
@@ -15,7 +16,8 @@ const getEndpointPath = getEndpointPathWith(
 export const getAllTours = async () =>
   axiosClient.get(getEndpointPath(API_PATHS.tours));
 
-export const getAllRoutes = async () => axiosClient.get("/api/routes");
+export const getAllRoutes = async () =>
+  axiosClient.get(getEndpointPath(API_PATHS.routes));
 
 export const userLogIn = async (email: string, password: string) =>
-  axiosClient.post("/api/login", { email, password });
+  axiosClient.post(getEndpointPath(API_PATHS.login), { email, password });
