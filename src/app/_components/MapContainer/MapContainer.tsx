29dfd7fa6458465621b459258258fd99fd655e6f
Luka Đurišić <luka.durisic123@gmail.com>
2025-04-28 09:05:06 +0200

    Map fix

--- src/app/_components/MapContainer/MapContainer.tsx
+++ src/app/_components/MapContainer/MapContainer.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "./MapContainer.module.css";
-// import Map from "@/app/_components/Map/Map";
+import Map from "@/app/_components/Map/Map";
+import { Route } from "@/app/_types";
 
-function MapContainer() {
-  return <div className={styles.temp}>{/* <Map /> */}</div>;
+function MapContainer({ routes }: { routes: Route[] }) {
+  return (
+    <div className={styles.temp}>
+      {" "}
+      <Map tourList={routes} />
+    </div>
+  );
 }
 
 export default MapContainer;
