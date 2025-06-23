4c6757f1882ee4ecf570a6d2b12fc10aa9290f9e
Luka Đurišić <luka.durisic123@gmail.com>
2025-06-23 19:26:29 +0200

    Settings finished

---
+++ src/app/_components/CustomInput/CustomInput.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import React, { useState, FocusEvent } from "react";
+import styles from "./CustomInput.module.css";
+import ShowIcon from "../../assets/show";
+import HideIcon from "../../assets/hide";
+import LockIcon from "../../assets/lock.svg";
+import Image from "next/image";
+
+interface CustomInputProps {
+  label: string;
+  type?: "text" | "password";
+  value: string;
+  onChange: (value: string) => void;
+  showError?: boolean;
+  placeholder?: string;
+  showInvalidError?: boolean;
+  showLock?: boolean;
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  label,
+  type = "text",
+  value,
+  onChange,
+  showError = false,
+  placeholder,
+  showInvalidError = false,
+  showLock,
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPassword = type === "password";
+  const labelVisible = isFocused || value.length > 0;
+  const hasError = showError || showInvalidError;
+
+  return (
+    <div className={styles.inputContainer}>
+      <label
+        className={`${labelVisible ? styles.label : styles.hiddenLabel} ${
+          hasError ? styles.error : ""
+        }`}
+      >
+        {label}
+      </label>
+
+      <div
+        className={`${
+          isPassword || showLock ? styles.passContainer : styles.inputWrapper
+        } ${isFocused ? styles.focused : ""} ${hasError ? styles.error : ""}`}
+      >
+        <input
+          type={isPassword && !showPassword ? "password" : "text"}
+          className={`${styles.input} ${
+            isPassword ? styles.inputPassword : ""
+          } ${hasError ? styles.error : ""} ${
+            hasError && isPassword ? styles.errorPass : ""
+          }`}
+          value={value}
+          placeholder={labelVisible ? "" : placeholder || label}
+          onChange={(e) => {
+            if (!showLock) {
+              onChange(e.target.value);
+            }
+          }}
+          onFocus={() => {
+            if (!showLock) {
+              setIsFocused(true);
+            }
+          }}
+          readOnly={showLock}
+          onBlur={(e: FocusEvent<HTMLInputElement>) => setIsFocused(false)}
+        />
+
+        {isPassword && value.length > 0 && (
+          <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className={styles.showPass}
+          >
+            {showPassword ? (
+              <HideIcon
+                height={22}
+                width={22}
+                stroke={hasError ? "#C11A1A" : "#9E9E9E"}
+              />
+            ) : (
+              <ShowIcon
+                height={22}
+                width={22}
+                stroke={hasError ? "#C11A1A" : "#9E9E9E"}
+              />
+            )}
+          </button>
+        )}
+
+        {showLock && (
+          <Image
+            src={LockIcon}
+            alt="lock"
+            height={22}
+            width={22}
+            className={styles.showPass}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CustomInput;
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -6,18 +6,50 @@ import UserMenu from "../_components/UserMenu/UserMenu";
 import Tabs from "../_components/Tabs/Tabs";
 import UserInfo from "../_components/UserInfo/UserInfo";
 import Button from "../_components/Button/Button";
+import CustomInput from "../_components/CustomInput/CustomInput";
 
 const userMock = {
   username: "Test user",
   profile_image:
     "/media/images/156-year_History_of_the_City_of_Victoria-image_3H6uIjj.jpg",
   date_added: "",
+};
+
+const userInputsMock = {
+  username: "Test user",
   country: "BiH",
+  firstName: "Test",
+  lastName: "user",
+  language: "Bosanski",
+  email: "testis.livi@gmail.comdf",
+  donationLink: "donate for 911",
 };
 
 function Settings() {
   const [selectedTab, setSelectedTab] = useState("edit profile");
   const [isFormFilled, setIsFormFilled] = useState(false);
+  const [userData, setUserData] = useState(
+    //   {
+    //   username: "",
+    //   firstName: "",
+    //   lastName: "",
+    //   country: "",
+    //   language: "",
+    //   email: "",
+    //   donationLink: "",
+    // }
+    userInputsMock
+  );
+
+  const [errors, setErrors] = useState<
+    Partial<Record<keyof typeof userData, boolean>>
+  >({});
+
+  const handleInputChange = (field: keyof typeof userData, value: string) => {
+    setUserData((prev) => ({ ...prev, [field]: value }));
+    setErrors((prev) => ({ ...prev, [field]: false }));
+  };
+
   return (
     <div className={styles.wrapper}>
       <Navbar />
@@ -47,10 +79,35 @@ function Settings() {
           <div className={styles.profileInfo}>
             <div className={styles.formContainer}>
               <div className={styles.profileTitle}>My account</div>
-              <div className={styles.form}>Form inputs</div>
+              <div className={styles.form}>
+                {(Object.keys(userData) as (keyof typeof userData)[]).map(
+                  (field) => (
+                    <CustomInput
+                      key={field}
+                      label={field
+                        .replace(/([A-Z])/g, " $1")
+                        .replace(/^./, (s) => s.toUpperCase())}
+                      value={userData[field]}
+                      onChange={(val) => handleInputChange(field, val)}
+                      showError={errors[field]}
+                      type="text"
+                      showLock={field === "username" || field === "email"}
+                    />
+                  )
+                )}
+              </div>
               <Button
                 variant="primary"
-                onClick={() => {}}
+                onClick={() => {
+                  const newErrors: typeof errors = {};
+                  for (const key in userData) {
+                    if (!userData[key as keyof typeof userData]) {
+                      newErrors[key as keyof typeof userData] = true;
+                    }
+                  }
+                  setErrors(newErrors);
+                  setIsFormFilled(Object.keys(newErrors).length === 0);
+                }}
                 className={!isFormFilled ? styles.disabled : undefined}
               >
                 {"CONTINUE"}
