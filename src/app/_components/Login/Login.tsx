7989b19dcfbd523f746433998de5d22f372ec6d5
Luka Đurišić <luka.durisic123@gmail.com>
2025-07-04 08:04:22 +0200

    dep fix

--- src/app/_components/Login/Login.tsx
+++ src/app/_components/Login/Login.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.css";
 import Button from "@/app/_components/Button/Button";
 import Image from "next/image";
